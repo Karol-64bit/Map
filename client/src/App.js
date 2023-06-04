38ab6ef07b329335b84e7290674c2efc38272f08
Jakub Marciniak <jmwarus2000@gmail.com>
2023-06-04 12:41:06 +0200

    Nowe pliki

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,31 @@
 // import React from 'react'
 import DataFetcher from './components/DataFetcher';
+import { GoogleMap } from '@react-google-maps';
 
 function App() {
 
   const data = DataFetcher();
 
+  function Map() {
+    return (
+      <GoogleMap
+        defaultZoom={10}
+        defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
+      />
+    )
+  }
+
   console.log(data)
   
   return (
     <div>
-      {data.map((item, i) => (
+      {/* {data.map((item, i) => (
         <div key={i}>
           <p>{item.id} {item.name}</p>
+          
         </div>
-      ))}
-
+      ))} */}
+      
     </div>
   );
 }
